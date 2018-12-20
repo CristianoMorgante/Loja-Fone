5af8fc9dc3ab8e67ce54c27e7ab50f53540ca7f8
Cristiano Morgante <cristianomorgante@hotmail.com>
2018-12-20 02:41:25 +0000

    Aula [015]

--- js/description-product.js
+++ js/description-product.js
@@ -1,12 +1,14 @@
-const $heart = window.document.querySelector(".-heart");
+const $heart = document.querySelector(".-heart");
+const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener('click', handleClick);
+$stars.forEach(function($star) {
+    $star.addEventListener('click', handleClick);
+});
 
 function handleClick() {
-    // if ($heart.classList.contains('-active')) {
-    //     $heart.classList.remove("-active");
-    // } else {
-    //     $heart.classList.add("-active");
-    // }
-    $heart.classList.toggle("-active");
-};
+    this.classList.toggle('-active');  
+}
+
+
+
